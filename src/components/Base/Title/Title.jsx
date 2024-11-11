d91d090f450fa268f30e534626b6aa1ba2e28f2a
Anastasiya.Honcharyk <honcharyk19@gmail.com>
2024-11-11 13:08:42 -0600

    Updates

--- src/components/Base/Title/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./Title.module.css";
-
-const Title = ({ titleText, extraClass = null }) => {
-  return <h1 className={`${style.title} ${extraClass}`}>{titleText}</h1>;
-};
-
-export default Title;
